147c9bcbe01f47af1cc6d36699aa0a25610b8026
bsafarov <behzod.safarov@esynergy.lv>
2023-05-15 16:47:37 +0500

    created google auth

--- ArzonOL/ArzonOL/Services/AuthService/Interfaces/ILoginService.cs
+++ ArzonOL/ArzonOL/Services/AuthService/Interfaces/ILoginService.cs
@@ -1,3 +1,4 @@
+using Google.Apis.Auth;
 using Microsoft.AspNetCore.Identity;
 namespace ArzonOL.Services.AuthService.Interfaces;
 
@@ -5,6 +6,7 @@ public interface ILoginService
 {
     Task<string> LogInAsync(string username, string password);
     Task<IdentityResult> LogOutAsync(Guid id, string password);
-    string CreateJwtToken(string username, string password, string email, string role);
+    string CreateJwtToken(string username, string email, string role, string userId);
     bool ValidateJwtToken(string token);
+    public Task<GoogleJsonWebSignature.Payload> VerifyGoogleToken(string provider, string idToken);
 }
